0380dd5e612646880390e6db91f412974d35e53d
Christopher Altona <caltona1@gmail.com>
2022-09-21 23:10:25 -0400

    Add airplay through shairport

---
+++ snapserver/src/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p /var/run/dbus/
+rm -rf /var/run/dbus/*
+dbus-daemon --print-address --system &
+sleep 5
+mkdir -p /var/run/avahi-daemon/
+rm -rf /var/run/avahi-daemon/*
+avahi-daemon &
+sleep 5
+snapserver
