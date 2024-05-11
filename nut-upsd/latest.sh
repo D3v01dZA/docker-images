01434c994028a09e9ed92c4f95d7908549489b51
Christopher Altona <caltona1@gmail.com>
2024-05-11 14:04:48 -0400

    Add aarch64

--- nut-upsd/latest.sh
+++ nut-upsd/latest.sh
@@ -10,4 +10,4 @@ fail() {
 
 #docker buildx create --name multiarch
 docker buildx use multiarch
-docker buildx build --platform linux/arm/v7 . -t d3v01d/nut-upsd:latest --push
+docker buildx build --platform linux/arm/v7 --platform linux/aarch64 . -t d3v01d/nut-upsd:latest --push
--- snapclient/latest.sh
+++ snapclient/latest.sh
@@ -12,3 +12,4 @@ cd src
 docker buildx use multiarch
 docker buildx build --platform linux/amd64 . -t d3v01d/snapclient:latest-amd64 --build-arg="ARCH=amd64" --push --progress=plain
 docker buildx build --platform linux/armhf . -t d3v01d/snapclient:latest-armhf --build-arg="ARCH=armhf" --push --progress=plain
+docker buildx build --platform linux/aarch64 . -t d3v01d/snapclient:latest-aarch64 --build-arg="ARCH=arm64" --push --progress=plain
