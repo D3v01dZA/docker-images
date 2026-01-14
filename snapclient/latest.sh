05d3b81831bceb08b7e52bfb6d24888ae6cf59d7
Christopher Altona <caltona1@gmail.com>
2026-01-14 13:30:31 -0500

    Update snapcast

--- snapclient/latest.sh
+++ snapclient/latest.sh
@@ -8,8 +8,22 @@ fail() {
     echo "Failed"
 }
 
+SHA256_AMD64="e3ef6a2c179e5c5743ee25e06345b2d602e51779c3d0c97b718b4eb625049f07"
+SHA256_ARM64="07cc9391a3dde10e243950958f276814be19c57efcd15b562424c9e3ada32a5b"
+
 cd src
 docker buildx use multiarch
-docker buildx build --platform linux/amd64 . -t d3v01d/snapclient:latest-amd64 --build-arg="ARCH=amd64" --push --progress=plain
-docker buildx build --platform linux/armhf . -t d3v01d/snapclient:latest-armhf --build-arg="ARCH=armhf" --push --progress=plain
-docker buildx build --platform linux/aarch64 . -t d3v01d/snapclient:latest-aarch64 --build-arg="ARCH=arm64" --push --progress=plain
+
+echo "Building amd64"
+docker buildx build --platform linux/amd64 . \
+    -t d3v01d/snapclient:latest-amd64 \
+    --build-arg="ARCH=amd64" \
+    --build-arg="SHA256=${SHA256_AMD64}" \
+    --push --progress=plain
+
+echo "Building aarch64"
+docker buildx build --platform linux/aarch64 . \
+    -t d3v01d/snapclient:latest-aarch64 \
+    --build-arg="ARCH=arm64" \
+    --build-arg="SHA256=${SHA256_ARM64}" \
+    --push --progress=plain
